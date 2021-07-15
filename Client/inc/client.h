fe2a0a64973a587f64092a7b2e7f126c72a85586
itided <roma.bvkn@gmail.com>
2021-07-15 17:48:28 +0300

    all project

---
+++ Client/inc/client.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "fmod.h"
+#include "fmod_codec.h"
+#include "fmod_dsp.h"
+#include "fmod_memoryinfo.h"
+#include "fmod_errors.h"
+#include "fmod_output.h"
+#include "uchat.h"
+#include "defines_client.h"
+
